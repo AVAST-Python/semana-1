3f18bc3af63a931f2e6727168eed65e00393b506
Alvaro Maceda <alvaro@alvaromaceda.es>
2023-09-29 07:39:51 +0200

    Working on isosceles

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -19,24 +19,70 @@ t.color('red')
 # TO-DO
 
 # Escalera
-t.lt(90)
-t.fd(100)
-t.rt(90)
-t.fd(150)
-t.rt(90)
-t.fd(50)
-t.lt(90)
-t.fd(150)
-t.rt(90)
-t.fd(50)
+# t.left(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(150)
+# t.right(90)
+# t.forward(50)
+# t.left(90)
+# t.forward(150)
+# t.right(90)
+# t.forward(50)
+
+
+# t.penup()
+# t.left(90)
+# t.forward(100)
+# t.pendown()
+
 
 # Triangulo equilátero
+# t.forward(100)
+# t.left(180-60)
+# t.forward(100)
+# t.left(180-60)
+# t.forward(100)
 
+
+# t.penup()
+# t.left(180-60)
+# t.forward(200)
+# t.pendown()
 
 # Triángulo isósceles
-# Hint: cos(60) = 1/2
+t.penup()
+t.setpos(-200,0)
+t.pendown()
 
 
+
+
+import math
+top_angle = 40
+base_angle = (180 - top_angle) / 2
+base = 100
+altura = base/2 *math.sin(base_angle)
+
+
+t.color('blue')
+t.forward(base/2)
+t.left(90)
+t.forward(altura)
+t.penup()
+t.setpos(-200,0)
+t.pendown()
+t.setheading(0)
+t.color('red')
+
+side = math.sqrt((base/2)**2 + altura**2)
+
+t.forward(base)
+t.left(180-base_angle)
+t.forward(side)
+t.left(180-top_angle)
+t.forward(side)
+t.left(180-base_angle)
 
 # turtle.done()
 # t.hideturtle()
